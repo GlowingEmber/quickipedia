5805015eb4b083b41dab40b7ae971232e23d5abe
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2024-03-09 17:50:00 -0800

    a few changes

--- quickipedia.js
+++ quickipedia.js
@@ -1,16 +1,28 @@
-let adjacentPages = (title) => {
-    fetch(
-       "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=" + title
-        ,{
-            method: "GET",
-        }
-    )
-.then((response) => response.json())
-.then((json_obj) => {
-    console.log(json_obj)
-})}
+async function node(title) {
+    let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&formatversion=2&titles=" + title)
+    let json = await node.json();
+    return json.query.pages[0].title;
+}
+async function children(title) {
+    let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&formatversion=2&titles=" + title)
+    let json = await node.json();
+    return json.query.pages[0].links;
+}
 
-adjacentPages("Donald Trump");
+let e_map = s_map = new Map();
 
-// node wikiferrari.js
-// time node wikiferrari.js > /dev/null 2>&1
+async function path(first, last) {
+    let it = 0;
+    s_map.set(await node(first), Math.floor(it/2)); it++;
+    e_map.set(await node(last), Math.floor(it/2)); it++;
+    let f_res = await children(first); let l_res = await children(last);
+    //console.log(map1.has("Donald Trump"))
+    console.log(s_map.get("Donald Trump"))
+    console.log(e_map.get("Barack Obama"))
+}
+
+async function compare() {
+
+}
+path("Donald Trump", "Barack Obama");
+// time node quickipedia.js > /dev/null 2>&1
