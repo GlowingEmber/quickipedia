cb116d5208352665967978f2ce16415f391ebd0c
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2024-03-09 18:38:58 -0800

    async function compare

--- quickipedia.js
+++ quickipedia.js
@@ -3,25 +3,36 @@ async function node(title) {
     let json = await node.json();
     return json.query.pages[0].title;
 }
+
 async function children(title) {
     let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&formatversion=2&titles=" + title)
     let json = await node.json();
     return json.query.pages[0].links;
 }
 
-let e_map = s_map = new Map();
+let map1 = new Map(); let map2 = new Map();
 
 async function path(first, last) {
-    let it = 0;
-    s_map.set(await node(first), Math.floor(it/2)); it++;
-    e_map.set(await node(last), Math.floor(it/2)); it++;
+    let iter = 0;
+    map1.set(await node(first), Math.floor(iter/2)); iter++;
+    map2.set(await node(last), Math.floor(iter/2)); iter++;
     let f_res = await children(first); let l_res = await children(last);
     //console.log(map1.has("Donald Trump"))
-    console.log(s_map.get("Donald Trump"))
-    console.log(e_map.get("Barack Obama"))
+    console.log(map1.get("Donald Trump"))
+    console.log(map2.get("Barack Obama"))
+    compare(first, last, "Donald Trump", iter)
 }
 
-async function compare() {
+async function compare(first, last, node, iter) {
+    let ends = [first, last];
+    let maps = [map1, map2];
+    let opposite = (iter+1) % 2;
+    if (maps[opposite].has(node)) {
+        console.log("YES!");
+        // return {};
+    } else {
+        console.log("NO!");
+    }
 
 }
 path("Donald Trump", "Barack Obama");
