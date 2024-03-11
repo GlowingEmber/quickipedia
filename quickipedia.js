6cbd3ca6b191104b8c6c1ffcf3435fd514643037
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2024-03-11 00:11:59 -0700

    working FIRST child completion

--- quickipedia.js
+++ quickipedia.js
@@ -12,28 +12,45 @@ async function children(title) {
 
 let map1 = new Map(); let map2 = new Map();
 
+async function completes(node, iter) {
+    let maps = [map1, map2];
+    let opposite = (iter+1) % 2;
+    return maps[opposite].has(node);
+    /*
+        let ends = [first, last];
+        let current = iter % 2;
+
+
+    if (maps[opposite].has(node)) {
+        console.log(map1.get(node) + map2.get(node));
+    }
+    else {
+        let f_res = await children(first);
+        f_res.forEach((child) => {
+            maps[current].set(child.title, iter);
+        });
+    }
+    */
+
+}
+
+async function add_children() {
+
+}
+
 async function path(first, last) {
     let iter = 0;
     map1.set(await node(first), Math.floor(iter/2)); iter++;
     map2.set(await node(last), Math.floor(iter/2)); iter++;
     let f_res = await children(first); let l_res = await children(last);
     //console.log(map1.has("Donald Trump"))
-    console.log(map1.get("Donald Trump"))
-    console.log(map2.get("Barack Obama"))
-    compare(first, last, "Donald Trump", iter)
-}
-
-async function compare(first, last, node, iter) {
-    let ends = [first, last];
-    let maps = [map1, map2];
-    let opposite = (iter+1) % 2;
-    if (maps[opposite].has(node)) {
-        console.log("YES!");
-        // return {};
-    } else {
-        console.log("NO!");
+    if (!(await completes(first, iter))) {
+        for (let x = 0; x < f_res.length; x++) {
+            if (await completes(f_res[x].title, iter)) {
+                console.log(f_res[x].title)
+            }
+        }
     }
-
-}
+}    
 path("Donald Trump", "Barack Obama");
 // time node quickipedia.js > /dev/null 2>&1
