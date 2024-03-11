28a82ca2e27995b88c9466b29a3813bf6b14d52a
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2024-03-11 02:27:55 -0700

    fixed multiple queries (now uses local variables)

--- quickipedia.js
+++ quickipedia.js
@@ -4,43 +4,62 @@ async function node_children(title) {
     return json.query.pages[0].links;
 }
 
-let map1 = new Map(); let map2 = new Map();
-let switched = current = opposite = descendent = 0;
-
-async function completes(node, maps, opposite) {
-    return maps[opposite].has(node);
+async function completes(node, maps, a) {
+    return maps[a.opposite].has(node);
 }
 
-async function traverse(node, maps, current, opposite, switched) {
-    if (await completes(node, maps, opposite)) {
-        return map1.get(node) + map2.get(node);
+async function traverse(node, maps, a) {
+    if (await completes(node, maps, a)) {
+        return maps[0].get(node) + maps[1].get(node);
     }
     let children = await node_children(node);
     for (const child of children) {
-        maps[current].set(child.title, descendent);
-        if (await completes(child.title, maps, opposite)) {
-            return map1.get(child.title) + map2.get(child.title);
+        maps[a.current].set(child.title, a.descendent);
+        if (await completes(child.title, maps, a)) {
+            return maps[0].get(child.title) + maps[1].get(child.title);
         }
     }
 }
 
-function switch_side() {
-    switched++; // number of times sides have switched
-    current = switched%2; // current map
-    opposite = (switched+1)%2; // opposite map
-    descendent = Math.floor(switched/2); // descendent depth
+function switch_side(a) {
+    a.switched++; // number of times sides have switched
+    a.current = a.switched%2; // current map
+    a.opposite = (a.switched-1)%2; // opposite map
+    a.descendent = Math.floor(a.switched/2); // descendent depth
 }
 
 async function path(first, last) {
-    map1.set(first, descendent); switch_side();
-    map2.set(last, descendent); switch_side();
+    let map1 = new Map(); let map2 = new Map();
+    let a = {
+        "switched": 0,
+        "current:": 0,
+        "descendent": 0,
+        "opposite": 1
+    }
+    map1.set(first, a.descendent); switch_side(a);
+    map2.set(last, a.descendent); switch_side(a);
     let maps = [map1, map2];
-    return await traverse(first, maps, current, opposite, switched);
+    return await traverse(first, maps, a);
 }
+
+
 
 async function run_tests() {
 console.log("Trump->Trump", await path("Donald Trump", "Donald Trump"));
 console.log("Trump->Obama", await path("Donald Trump", "Barack Obama"));
+console.log("Trump->Thunberg", await path("Donald Trump", "Greta Thunberg"));
 }
 run_tests();
 // time node quickipedia.js > /dev/null 2>&1
+
+// current
+// 01234567
+// 01010101
+
+// opposite
+// 01234567
+// 10101010
+
+// descendent
+// 01234567
+// 00112233
