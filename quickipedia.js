a17a2ad89252e197acdc855193fe342aba3ed93d
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2024-03-11 01:47:40 -0700

    better first children

--- quickipedia.js
+++ quickipedia.js
@@ -1,56 +1,46 @@
-async function node(title) {
-    let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&formatversion=2&titles=" + title)
-    let json = await node.json();
-    return json.query.pages[0].title;
-}
-
-async function children(title) {
+async function node_children(title) {
     let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&formatversion=2&titles=" + title)
     let json = await node.json();
     return json.query.pages[0].links;
 }
 
 let map1 = new Map(); let map2 = new Map();
+let switched = current = opposite = descendent = 0;
 
-async function completes(node, iter) {
-    let maps = [map1, map2];
-    let opposite = (iter+1) % 2;
+async function completes(node, maps, opposite) {
     return maps[opposite].has(node);
-    /*
-        let ends = [first, last];
-        let current = iter % 2;
-
-
-    if (maps[opposite].has(node)) {
-        console.log(map1.get(node) + map2.get(node));
-    }
-    else {
-        let f_res = await children(first);
-        f_res.forEach((child) => {
-            maps[current].set(child.title, iter);
-        });
-    }
-    */
-
 }
 
-async function add_children() {
+async function traverse(node, maps, current, opposite, switched) {
+    if (await completes(node, maps, opposite)) {
+        return map1.get(node) + map2.get(node);
+    }
+    let children = await node_children(node);
+    for (const child of children) {
+        maps[current].set(child.title, descendent);
+        if (await completes(child.title, maps, opposite)) {
+            return map1.get(child.title) + map2.get(child.title);
+        }
+    }
+}
 
+function switch_side() {
+    switched++; // number of times sides have switched
+    current = switched%2; // current map
+    opposite = (switched+1)%2; // opposite map
+    descendent = Math.floor(switched/2); // descendent depth
 }
 
 async function path(first, last) {
-    let iter = 0;
-    map1.set(await node(first), Math.floor(iter/2)); iter++;
-    map2.set(await node(last), Math.floor(iter/2)); iter++;
-    let f_res = await children(first); let l_res = await children(last);
-    //console.log(map1.has("Donald Trump"))
-    if (!(await completes(first, iter))) {
-        for (let x = 0; x < f_res.length; x++) {
-            if (await completes(f_res[x].title, iter)) {
-                console.log(f_res[x].title)
-            }
-        }
-    }
-}    
-path("Donald Trump", "Barack Obama");
+    map1.set(first, descendent); switch_side();
+    map2.set(last, descendent); switch_side();
+    let maps = [map1, map2];
+    return await traverse(first, maps, current, opposite, switched);
+}
+
+async function run_tests() {
+console.log("Trump->Trump", await path("Donald Trump", "Donald Trump"));
+console.log("Trump->Obama", await path("Donald Trump", "Barack Obama"));
+}
+run_tests();
 // time node quickipedia.js > /dev/null 2>&1
