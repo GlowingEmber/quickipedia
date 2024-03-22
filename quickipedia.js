0678ed66a7e70b0801cb13b7b2c885a276224bdf
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2024-03-22 00:49:45 -0700

    pageid setup (not done)

--- quickipedia.js
+++ quickipedia.js
@@ -3,18 +3,22 @@ class Shortest_Path {
     constructor(first, last) {
         this.first = first; this.last = last;
         this.map1 = new Map(); this.map2 = new Map();
+        this.maps = [this.map1, this.map2];
         this.state = {
             "switched": 0,
             "current_tree": 0,
             "depth": 0,
             "opposite_tree": 1
         }
-        this.map1.set(first, ({depth: 0, parents: []}));
-        this.map2.set(last, ({depth: 0, parents: []}));
-        this.maps = [this.map1, this.map2];
-        this.map1_leaves = [first]; this.map2_leaves = [last];
-        this.leaves = [this.map1_leaves, this.map2_leaves];
+    }
 
+    async setup() {
+        let first_id = await this.node_id(this.first);
+        let last_id = await this.node_id(this.last);
+        this.map1.set(first_id, ({depth: 0, parents: []}));
+        this.map2.set(last_id, ({depth: 0, parents: []}));
+        this.map1_leaves = [first_id]; this.map2_leaves = [last_id];
+        this.leaves = [this.map1_leaves, this.map2_leaves];
     }
 
     set_map(node) {
@@ -30,10 +34,16 @@ class Shortest_Path {
         this.state.depth = Math.floor(this.state.switched/2); // descendant depth
     }
 
-    async node_children(title) {
-        let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&formatversion=2&titles=" + title)
+    async node_id(title) {
+        let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&formatversion=2&titles=" + title);
         let json = await node.json();
-        return json.query.pages[0].links.map(page => page.title);
+        return json.query.pages[0].pageid;
+    }
+
+    async node_children(id) {
+        let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&generator=links&format=json&formatversion=2&prop=info&pllimit=500&pageids=" + id);
+        let json = await node.json();
+        return json.query.pages.map(page => page.pageid);
     }
     
 
@@ -42,8 +52,8 @@ class Shortest_Path {
     }
 
     async traverse(node) {
-
-        while (this.state.switched < 3) {
+        await this.setup();
+        while (this.state.switched < 1) {
             // console.log(this.leaves[this.state.current_tree])
             let leaves = this.leaves[this.state.current_tree];
             this.leaves[this.state.current_tree] = [];
@@ -59,7 +69,7 @@ class Shortest_Path {
                 this.leaves[this.state.current_tree] = 
                 this.leaves[this.state.current_tree].concat(await this.node_children(leaf));
             }
-            //console.log(this.leaves[this.state.current_tree])
+            // console.log(this.leaves[this.state.current_tree])
             this.switch_side();
         }
     }
