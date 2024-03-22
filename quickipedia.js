9766aa3bb4520e58c45018aad846d6f82b9db601
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2024-03-22 01:13:07 -0700

    gpll fix

--- quickipedia.js
+++ quickipedia.js
@@ -41,8 +41,9 @@ class Shortest_Path {
     }
 
     async node_children(id) {
-        let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&generator=links&format=json&formatversion=2&prop=info&pllimit=500&pageids=" + id);
+        let node = await fetch("https://en.wikipedia.org/w/api.php?action=query&generator=links&format=json&formatversion=2&gpllimit=500&pageids=" + id);
         let json = await node.json();
+        console.log(json.query.pages.map(page => page.pageid));
         return json.query.pages.map(page => page.pageid);
     }
     
